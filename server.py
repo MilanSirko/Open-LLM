7f72c7f8c217491556a8161d8a013c5970dc2960
Milan Sirko <sirkomilan570@gmail.com>
2026-08-07 16:46:15 +0200

    add CORS allowed origins and update GitHub link

--- server.py
+++ server.py
@@ -30,7 +30,7 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://open-llm-ochre.vercel.app", "http://localhost:5500"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
